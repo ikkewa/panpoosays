7e8f230f562ad1beb00f8025fc07664a73f4c67f
IkkeWa <xikkewa@gmail.com>
2015-06-24 16:17:36 +0200

    fix(): prepare for config.tpl.js to remove fixed url in config

--- app/js/config.js
+++ app/js/config.js
@@ -1,5 +1,5 @@
 
-var publicUrl = 'bit.ly/1JiGPKz'
+var publicUrl = '<enter public url here>';
 var localUrl = '127.0.0.1';
 var url = process.env.NODE_ENV === 'development' ? localUrl : publicUrl;
 
