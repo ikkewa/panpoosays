33ba86e8425d65a15bba25ef98b0c41c58371178
IkkeWa <xikkewa@gmail.com>
2015-06-24 16:17:52 +0200

    fix(): remove config.js

--- app/js/config.js
+++
@@ -1,8 +0,0 @@
-
-var publicUrl = '<enter public url here>';
-var localUrl = '127.0.0.1';
-var url = process.env.NODE_ENV === 'development' ? localUrl : publicUrl;
-
-module.exports = {
-  socketUrl: 'http://' + url + ':8080'
-};
